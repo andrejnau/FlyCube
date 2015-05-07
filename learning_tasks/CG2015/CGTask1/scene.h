8aa6604b6d2b3ef43dc400c104b8a561bbcd17c2
softmob <softmob@mail.ru>
2015-05-07 09:33:54 +0300

    CGTask1 : fixed physics

--- learning_tasks/CG2015/CGTask1/scene.h
+++ learning_tasks/CG2015/CGTask1/scene.h
@@ -10,6 +10,8 @@
 #include <SOIL.h>
 #include <camera.h>
 
+#define ONE_LOOP
+
 #include <glm/gtc/matrix_transform.hpp>
 
 class tScenes : public SceneBase, public SingleTon < tScenes >
@@ -44,7 +46,7 @@ public:
 			m_camera.Update();
 		}
 
-		glm::vec3 a(-1.0f, 1.0f+eps, 1.0f);
+		glm::vec3 a(-1.0f, 1.0f + eps, 1.0f);
 		glm::vec3 b(1.0f, 1.0f + eps, 1.0f);
 		glm::vec3 c(1.0f, 1.0f + eps, -1.0f);
 		glm::vec3 d(-1.0f, 1.0f + eps, -1.0f);
@@ -65,7 +67,7 @@ public:
 			auto vec = modelOfMemoryL[i - 1].vertices;
 			for (int j = 0; j < vec.size(); ++j)
 			{
-				glm::mat3 r = glm::mat3(glm::rotate(glm::mat4(1.0f), -acos(-1.0f) / 2.0f, this->axis_z));
+				glm::mat3 r = glm::mat3(glm::rotate(glm::mat4(1.0f), acos(-1.0f) / 2.0f, this->axis_z));
 				vec[j] = r * vec[j];
 			}
 			modelOfMemoryL[i].vertices = vec;
@@ -77,7 +79,7 @@ public:
 		}
 
 		//modelOfMemoryL[0].p
-		
+
 		modelOfFile.reset("parallelepiped.obj");
 		return true;
 	}
@@ -98,30 +100,46 @@ public:
 		int64_t elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 		start = std::chrono::system_clock::now();
 
-		float dt = elapsed / 1500.0f;
+		float dt = std::min(0.001f, elapsed / 1500.0f);
 
-		angle += dt;
+#ifdef ONE_LOOP
+		if (!full_turn)
+#endif
+			angle += dt;
 
 		glm::vec3 g(-1.0f, 1.0f + eps, -1.0f);
-		glm::mat3 rotate = glm::mat3(glm::rotate(glm::mat4(1.0f), -angle, this->axis_z));
+		glm::mat3 rotate = glm::mat3(glm::rotate(glm::mat4(1.0f), angle, this->axis_z));
 
-		for (int i = 0; i < this->modelOfMemoryL.size(); ++i)
+#ifdef ONE_LOOP
+		for (int i = 0; i < 4; ++i)
+#else
+		for (int i = 0; i < 1000; ++i)
+#endif
 		{
-			if (glm::distance(g, rotate * modelOfMemoryL[i].vertices[0]) < 1e-1)
+			float m_angle = i * acos(-1.0) / 2.0f;
+			if (fabs(m_angle - angle) < eps)
 			{
-				cur_id = i;
+				cur_id = i % 4;
 				break;
 			}
+
 		}
 
-		if (cur_id == this->modelOfMemoryL.size() - 1)
+		if (angle > 2.0 * acos(-1.0f))
+		{
 			full_turn = true;
+		}
 
-		lenta_a.x += dt;
-		lenta_b.x += dt;
+#ifdef ONE_LOOP
+		if (!full_turn)
+#endif
+		{
+			lenta_a.x += dt;
+			lenta_b.x += dt;
+		}
 
-		glm::vec3 lenta_c = rotate * modelOfMemoryL[cur_id].vertices[1];
-		glm::vec3 lenta_d = rotate * modelOfMemoryL[cur_id].vertices[0];
+		glm::vec3 lenta_c = glm::inverse(rotate) * modelOfMemoryL[cur_id].vertices[1];
+		glm::vec3 lenta_d = glm::inverse(rotate) * modelOfMemoryL[cur_id].vertices[0];
 
 		modelLenta.vertices.clear();
 		modelLenta.vertices.push_back(lenta_d);
@@ -132,7 +150,7 @@ public:
 		modelLenta.vertices.push_back(lenta_b);
 		modelLenta.vertices.push_back(lenta_d);
 		modelLenta.init_vao();
-	
+
 		draw_obj();
 	}
 
@@ -159,7 +177,7 @@ public:
 		glUniformMatrix4fv(shaderLight.loc_MVP, 1, GL_FALSE, glm::value_ptr(Matrix));
 
 		glUniform3fv(shaderLight.loc_lightPosition, 1, glm::value_ptr(lightPosition));
-		glUniform3fv(shaderLight.loc_camera, 1, glm::value_ptr(camera));	
+		glUniform3fv(shaderLight.loc_camera, 1, glm::value_ptr(camera));
 		glUniform4f(shaderLight.loc_color, 0.5f, 0.5f, 0.5f, 1.0f);
 
 		glBindVertexArray(modelOfFile.vaoObject);
@@ -168,7 +186,11 @@ public:
 
 		glUniform4f(shaderLight.loc_color, 1.0f, 0.0f, 0.0f, 1.0f);
 
+#ifdef ONE_LOOP
+		for (int i = cur_id + 1; i < modelOfMemoryL.size(); ++i)
+#else
 		for (int i = 0; i < modelOfMemoryL.size(); ++i)
+#endif
 		{
 			glBindVertexArray(modelOfMemoryL[i].vaoObject);
 			glDrawArrays(GL_TRIANGLES, 0, modelOfMemoryL[i].vertices.size());
@@ -180,15 +202,15 @@ public:
 
 		glBindVertexArray(modelLenta.vaoObject);
 		glDrawArrays(GL_TRIANGLES, 0, modelLenta.vertices.size());
-		glBindVertexArray(0);		
+		glBindVertexArray(0);
 	}
-	
+
 	virtual void resize(int x, int y, int width, int height)
 	{
 		glViewport(x, y, width, height);
 		m_width = width;
 		m_height = height;
-		m_camera.SetViewport(x, y, width, height);		
+		m_camera.SetViewport(x, y, width, height);
 	}
 
 	virtual void destroy()
