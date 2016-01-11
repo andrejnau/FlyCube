359a6aa0f590fe23551e0712cf965a5059f1b317
softmob <softmob@mail.ru>
2016-01-12 01:54:11 +0300

    update 3rdparty lib

--- cmake/3rdparty/glbinding.cmake
+++ cmake/3rdparty/glbinding.cmake
@@ -1,10 +1,10 @@
-option(BUILD_SHARED_LIBS     "Build shared instead of static libraries."              OFF)
 option(OPTION_SELF_CONTAINED "Create a self-contained install with all dependencies." OFF)
 option(OPTION_BUILD_TESTS    "Build tests."                                           OFF)
 option(OPTION_BUILD_DOCS     "Build documentation."                                   OFF)
 option(OPTION_BUILD_EXAMPLES "Build examples."                                        OFF)
 option(OPTION_BUILD_TOOLS    "Build tools."                                           OFF)
 option(OPTION_EXTENSIVE_META "Provide extensive meta information for bidirectional conversion between String and OpenGL types" OFF)
+
 add_subdirectory(${PROJECT_ROOT}/3rdparty/glbinding-master)
 
 set_target_properties(generate PROPERTIES FOLDER "3rdparty/glbinding")
