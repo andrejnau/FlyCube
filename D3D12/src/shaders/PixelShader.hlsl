85f95b52e851de474022fec80bb39ae93725ead7
Andrew Naumov <softmob@mail.ru>
2016-07-04 22:57:38 +0300

    [DX12] simple lighting

--- D3D12/src/shaders/PixelShader.hlsl
+++ D3D12/src/shaders/PixelShader.hlsl
@@ -1,15 +1,13 @@
 struct VS_OUTPUT
 {
     float4 pos: SV_POSITION;
+    float3 fragPos : POSITION;
     float3 normal : NORMAL;
     float2 texCoord: TEXCOORD;
     float3 tangent: TANGENT;
     float3 bitangent: BITANGENT;
-};
-
-cbuffer ConstantBuffer : register(b1)
-{
-    float4 colorMultiplier;
+    float3 lightPos : POSITION_LIGHT;
+    float3 viewPos : POSITION_VIEW;
 };
 
 Texture2D g_texture : register(t0);
@@ -17,6 +15,15 @@ SamplerState g_sampler : register(s0);
 
 float4 main(VS_OUTPUT input) : SV_TARGET
 {
-   //return g_texture.Sample(g_sampler, input.texCoord);
-   return float4(input.normal, 1.0);
+    // Diffuse
+    float3 lightDir = normalize(input.lightPos - input.fragPos);
+    float diff = max(dot(lightDir, input.normal), 0.0);
+    float3 diffuse = g_texture.Sample(g_sampler, input.texCoord) * diff;
+
+    // Specular
+    float3 viewDir = normalize(input.viewPos - input.fragPos);
+    float3 reflectDir = reflect(-lightDir, input.normal);
+    float spec = pow(max(dot(input.normal, reflectDir), 0.0), 32.0);
+    float3 specular = float3(1.0, 1.0, 1.0) * spec;
+    return float4(diffuse + specular, 1.0);
 }
--- D3D12/src/shaders/VertexShader.hlsl
+++ D3D12/src/shaders/VertexShader.hlsl
@@ -10,10 +10,13 @@ struct VS_INPUT
 struct VS_OUTPUT
 {
     float4 pos: SV_POSITION;
+    float3 fragPos : POSITION;
     float3 normal : NORMAL;
     float2 texCoord: TEXCOORD;
     float3 tangent: TANGENT;
     float3 bitangent: BITANGENT;
+    float3 lightPos : POSITION_LIGHT;
+    float3 viewPos : POSITION_VIEW;
 };
 
 cbuffer ConstantBuffer : register(b0)
@@ -29,13 +32,21 @@ cbuffer ConstantBuffer : register(b0)
 VS_OUTPUT main(VS_INPUT input)
 {
     VS_OUTPUT output;
-    output.pos = mul(float4(input.pos, 1.0), mvp);
+
+    float4 newPosition = float4(input.pos, 1.0f);
+    newPosition = mul(newPosition, model);
+    output.fragPos = newPosition;
+    newPosition = mul(newPosition, view);
+    newPosition = mul(newPosition, projection);
+
+    output.pos = newPosition;
     output.texCoord = input.texCoord;
 
-    float3x3 normalMatrix = (float3x3)transpose(model);
-    float3 N = normalize(mul(input.normal, normalMatrix));
+    output.lightPos = lightPos;
+    output.viewPos = viewPos;
 
-    output.normal = N;
+    output.normal = mul(input.normal, (float3x3)model);
+    output.normal = normalize(output.normal);
     output.tangent = input.tangent;
     output.bitangent = input.bitangent;
     return output;
