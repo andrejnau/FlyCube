8b7e3d4e1fa2464b956921c423c19eccfe99ad07
Andrew Naumov <softmob@mail.ru>
2017-09-09 13:03:11 +0300

    add common getting model size

--- src/Modules/Geometry/Geometry.h
+++ src/Modules/Geometry/Geometry.h
@@ -57,6 +57,7 @@ struct IModel
 {
     virtual IMesh& GetNextMesh() = 0;
     virtual BoundBox& GetBoundBox() = 0;
+    virtual const BoundBox& GetBoundBox() const = 0;
 };
 
 class ModelLoader
@@ -97,9 +98,40 @@ struct Model : IModel
         return bound_box;
     }
 
+    virtual const BoundBox& GetBoundBox() const override
+    {
+        return bound_box;
+    }
+
     BoundBox bound_box;
     std::vector<Mesh> meshes;
 
 private:
     ModelLoader m_model_loader;
+};
+
+struct ModelSize
+{
+    ModelSize(const IModel& model)
+    {
+        const BoundBox& bound_box = model.GetBoundBox();
+        z_width = (bound_box.z_max - bound_box.z_min);
+        y_width = (bound_box.y_max - bound_box.y_min);
+        x_width = (bound_box.x_max - bound_box.x_min);
+        model_width = std::max({ z_width, y_width, x_width });
+        scale = 1.0 / model_width;
+        model_width *= scale;
+        offset_x = (bound_box.x_max + bound_box.x_min) / 2.0f;
+        offset_y = (bound_box.y_max + bound_box.y_min) / 2.0f;
+        offset_z = (bound_box.z_max + bound_box.z_min) / 2.0f;
+    }
+
+    float z_width;
+    float y_width;
+    float x_width;
+    float model_width;
+    float scale;
+    float offset_x;
+    float offset_y;
+    float offset_z;
 };
