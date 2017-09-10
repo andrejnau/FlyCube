b7d993b71704877d9f02369961b9c1efb41bcd5d
Andrew Naumov <softmob@mail.ru>
2017-09-10 20:42:19 +0300

    reorder member and func

--- src/Apps/D3D11/Shaders.h
+++ src/Apps/D3D11/Shaders.h
@@ -1,50 +1,174 @@
 ﻿#pragma once
 
 #include <d3d11.h>
-
-#include <d3d11.h>
-#include <DXGI1_4.h>
-#include <D3Dcompiler.h>
-#include <DirectXMath.h>
 #include <wrl/client.h>
-#include <glm/glm.hpp>
-#include <glm/gtx/transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <D3Dcompiler.h>
 
-#include <memory>
+#include <map>
 #include <string>
+#include <vector>
 
-#include <Geometry/Geometry.h>
-#include <Util.h>
 #include <Utility.h>
+#include <Geometry/Geometry.h>
 #include <Utilities/FileUtility.h>
 
-#include "DX11Geometry.h"
+enum class ShaderType
+{
+    kVertex,
+    kPixel
+};
 
-using namespace Microsoft::WRL;
-using namespace DirectX;
+class UniformProxy
+{
+public:
+    UniformProxy(char* data, size_t size)
+        : m_data(data)
+        , m_size(size)
+    {
+    }
+
+    template<typename T>
+    void operator=(const T& value)
+    {
+        assert(sizeof(T) == m_size);
+        *reinterpret_cast<T*>(m_data) = value;
+    }
+
+private:
+    char* m_data;
+    size_t m_size;
+};
+
+class CBuffer
+{
+public:
+    CBuffer(ComPtr<ID3D11Device>& device, ShaderType shader_type, ComPtr<ID3D11ShaderReflection>& reflector, UINT index)
+        : m_slot(index)
+        , m_shader_type(shader_type)
+    {
+        ID3D11ShaderReflectionConstantBuffer* cbuffer = nullptr;
+        cbuffer = reflector->GetConstantBufferByIndex(index);
+
+        D3D11_SHADER_BUFFER_DESC bdesc = {};
+        cbuffer->GetDesc(&bdesc);
+
+        m_buffer = CreateBuffer(device, bdesc.Size);
+        m_data.resize(bdesc.Size);
+
+        for (UINT i = 0; i < bdesc.Variables; ++i)
+        {
+            ID3D11ShaderReflectionVariable* variable = nullptr;
+            variable = cbuffer->GetVariableByIndex(i);
+
+            D3D11_SHADER_VARIABLE_DESC orig_vdesc;
+            variable->GetDesc(&orig_vdesc);
+
+            VDesc& vdesc = m_vdesc[orig_vdesc.Name];
+            vdesc.size = orig_vdesc.Size;
+            vdesc.offset = orig_vdesc.StartOffset;
+        }
+    }
+
+    UniformProxy Uniform(const std::string& name)
+    {
+        if (m_vdesc.count(name))
+        {
+            auto& desc = m_vdesc[name];
+            return UniformProxy(m_data.data() + desc.offset, desc.size);
+        }
+
+        throw std::exception("name not found");
+    }
+
+    void Update(ComPtr<ID3D11DeviceContext>& device_context)
+    {
+        device_context->UpdateSubresource(m_buffer.Get(), 0, nullptr, m_data.data(), 0, 0);
+    }
+
+    void SetOnPipeline(ComPtr<ID3D11DeviceContext>& device_context)
+    {
+        if (m_shader_type == ShaderType::kVertex)
+            device_context->VSSetConstantBuffers(m_slot, 1, m_buffer.GetAddressOf());
+        else
+            device_context->PSSetConstantBuffers(m_slot, 1, m_buffer.GetAddressOf());
+    }
+
+private:
+    ComPtr<ID3D11Buffer> CreateBuffer(ComPtr<ID3D11Device>& device, UINT buffer_size)
+    {
+        ComPtr<ID3D11Buffer> buffer;
+        D3D11_BUFFER_DESC cbbd;
+        ZeroMemory(&cbbd, sizeof(D3D11_BUFFER_DESC));
+        cbbd.Usage = D3D11_USAGE_DEFAULT;
+        cbbd.ByteWidth = buffer_size;
+        cbbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+        cbbd.CPUAccessFlags = 0;
+        cbbd.MiscFlags = 0;
+        ASSERT_SUCCEEDED(device->CreateBuffer(&cbbd, nullptr, &buffer));
+        return buffer;
+    }
+
+    struct VDesc
+    {
+        size_t size;
+        size_t offset;
+    };
+
+    std::map<std::string, VDesc> m_vdesc;
+    UINT m_slot;
+    ShaderType m_shader_type;
+    ComPtr<ID3D11Buffer> m_buffer;
+    std::vector<char> m_data;
+};
 
 class DXShader
 {
 public:
-
-    enum class ShaderType
+    DXShader(ComPtr<ID3D11Device>& device, const std::string& vertex, const std::string& pixel)
     {
-        kVertex,
-        kPixel
-    };
+        m_vertex_shader_buffer = CompileShader(device, vertex, "main", "vs_5_0");
+        ASSERT_SUCCEEDED(device->CreateVertexShader(m_vertex_shader_buffer->GetBufferPointer(), m_vertex_shader_buffer->GetBufferSize(), nullptr, &vertex_shader));
+
+        m_pixel_shader_buffer = CompileShader(device, pixel, "main", "ps_5_0");
+        ASSERT_SUCCEEDED(device->CreatePixelShader(m_pixel_shader_buffer->GetBufferPointer(), m_pixel_shader_buffer->GetBufferSize(), nullptr, &pixel_shader));
+
+        D3D11_INPUT_ELEMENT_DESC layout[] =
+        {
+            { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(DX11Mesh::Vertex, position), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(DX11Mesh::Vertex, normal), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, offsetof(DX11Mesh::Vertex, texCoords), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(DX11Mesh::Vertex, tangent), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        };
+
+        ASSERT_SUCCEEDED(device->CreateInputLayout(layout, ARRAYSIZE(layout), m_vertex_shader_buffer->GetBufferPointer(),
+            m_vertex_shader_buffer->GetBufferSize(), &input_layout));
+
+        ParseVariable(device, ShaderType::kVertex, m_vertex_shader_buffer, m_vs_buffers);
+        ParseVariable(device, ShaderType::kPixel, m_pixel_shader_buffer, m_ps_buffers);
+    }
+
+    CBuffer& GetVSCBuffer(const std::string& name)
+    {
+        auto it = m_vs_buffers.find(name);
+        if (it != m_vs_buffers.end())
+            return it->second;
+        throw std::exception("name not found");
+    }
+
+    CBuffer& GetPSCBuffer(const std::string& name)
+    {
+        auto it = m_ps_buffers.find(name);
+        if (it != m_ps_buffers.end())
+            return it->second;
+        throw std::exception("name not found");
+    }
 
     ComPtr<ID3D11VertexShader> vertex_shader;
     ComPtr<ID3D11PixelShader> pixel_shader;
-    ComPtr<ID3DBlob> vertex_shader_buffer;
-    ComPtr<ID3DBlob> pixel_shader_buffer;
     ComPtr<ID3D11InputLayout> input_layout;
 
-     ComPtr<ID3DBlob> CompileShader(
-        ComPtr<ID3D11Device> device, 
-        const std::string& shader_path,
-        const std::string& entrypoint,
-        const std::string& target)
+private:
+    ComPtr<ID3DBlob> CompileShader(ComPtr<ID3D11Device>& device, const std::string& shader_path, const std::string& entrypoint, const std::string& target)
     {
         ComPtr<ID3DBlob> errors;
         ComPtr<ID3DBlob> shader_buffer;
@@ -61,125 +185,6 @@ public:
         return shader_buffer;
     }
 
-    struct CBuffer
-    {
-        class UniformProxy
-        {
-        public:
-            UniformProxy(CBuffer& self, const std::string& name)
-                : m_self(self)
-                , m_name(name)
-            {
-            }
-
-            template<typename T>
-            void operator=(const T& value)
-            {
-                auto& desc = m_self.m_vdesc[m_name];
-                *reinterpret_cast<T*>(m_self.m_data.data() + desc.offset) = value;
-            }
-
-            const std::string& m_name;
-            CBuffer& m_self;
-        };
-
-        ComPtr<ID3D11Buffer> CreateBuffer(ComPtr<ID3D11Device>& device, UINT buffer_size)
-        {
-            ComPtr<ID3D11Buffer> buffer;
-            D3D11_BUFFER_DESC cbbd;
-            ZeroMemory(&cbbd, sizeof(D3D11_BUFFER_DESC));
-            cbbd.Usage = D3D11_USAGE_DEFAULT;
-            cbbd.ByteWidth = buffer_size;
-            cbbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-            cbbd.CPUAccessFlags = 0;
-            cbbd.MiscFlags = 0;
-            ASSERT_SUCCEEDED(device->CreateBuffer(&cbbd, nullptr, &buffer));
-            return buffer;
-        }
-
-        ShaderType m_shader_type;
-
-        CBuffer(ComPtr<ID3D11Device>& device, ShaderType shader_type, ComPtr<ID3D11ShaderReflection>& reflector, UINT Index)
-            : m_slot(Index)
-            , m_shader_type(shader_type)
-        {
-            ID3D11ShaderReflectionConstantBuffer* cbuffer = nullptr;
-            cbuffer = reflector->GetConstantBufferByIndex(Index);
-
-            D3D11_SHADER_BUFFER_DESC bdesc = {};
-            cbuffer->GetDesc(&bdesc);
-            m_name = bdesc.Name;
-
-            m_buffer = CreateBuffer(device, bdesc.Size);
-            m_data.resize(bdesc.Size);
-
-            for (UINT i = 0; i < bdesc.Variables; ++i)
-            {
-                ID3D11ShaderReflectionVariable* variable = nullptr;
-                variable = cbuffer->GetVariableByIndex(i);
-
-                D3D11_SHADER_VARIABLE_DESC vdesc;
-                variable->GetDesc(&vdesc);
-
-                auto& d = m_vdesc[vdesc.Name];
-                d.size = vdesc.Size;
-                d.offset = vdesc.StartOffset;
-            }
-        }
-
-        UniformProxy Uniform(const std::string& name)
-        {
-            if (m_vdesc.count(name))
-                return UniformProxy(*this, name);
-            throw std::exception("name not found");
-        }
-
-        void Update(ComPtr<ID3D11DeviceContext>& device_context)
-        {
-            device_context->UpdateSubresource(m_buffer.Get(), 0, nullptr, m_data.data(), 0, 0);
-        }
-
-        void SetOnPipeline(ComPtr<ID3D11DeviceContext>& device_context)
-        {
-            if (m_shader_type == ShaderType::kVertex)
-                device_context->VSSetConstantBuffers(m_slot, 1, m_buffer.GetAddressOf());
-            else
-                device_context->PSSetConstantBuffers(m_slot, 1, m_buffer.GetAddressOf());
-        }
-
-        ComPtr<ID3D11Buffer> m_buffer;
-        std::vector<char> m_data;
-        UINT m_slot = 0;
-
-        struct VDesc
-        {
-            size_t size;
-            size_t offset;
-        };
-
-        std::map<std::string, VDesc> m_vdesc;
-        std::string m_name;
-    };
-
-    std::map<std::string, CBuffer> vs_buffers;
-    std::map<std::string, CBuffer> ps_buffers;
-
-    CBuffer& GetVSCBuffer(const std::string& name)
-    {
-        auto it = vs_buffers.find(name);
-        if (it != vs_buffers.end())
-            return it->second;
-        throw std::exception("name not found");
-    }
-
-    CBuffer& GetPSCBuffer(const std::string& name)
-    {
-        auto it = ps_buffers.find(name);
-        if (it != ps_buffers.end())
-            return it->second;
-        throw std::exception("name not found");
-    }
-
     void ParseVariable(ComPtr<ID3D11Device>& device, ShaderType shader_type, ComPtr<ID3DBlob>& shader_buffer, std::map<std::string, CBuffer>& buffers)
     {
         ComPtr<ID3D11ShaderReflection> reflector;
@@ -192,33 +197,15 @@ public:
         {
             ID3D11ShaderReflectionConstantBuffer* buffer = nullptr;
             buffer = reflector->GetConstantBufferByIndex(i);
-
             D3D11_SHADER_BUFFER_DESC bdesc = {};
             buffer->GetDesc(&bdesc);
             buffers.emplace(bdesc.Name, CBuffer(device, shader_type, reflector, i));
         }
     }
 
-    DXShader(ComPtr<ID3D11Device> device, const std::string& vertex, const std::string& pixel)
-    {
-        vertex_shader_buffer = CompileShader(device, vertex, "main", "vs_5_0");
-        ASSERT_SUCCEEDED(device->CreateVertexShader(vertex_shader_buffer->GetBufferPointer(), vertex_shader_buffer->GetBufferSize(), nullptr, &vertex_shader));
+    ComPtr<ID3DBlob> m_vertex_shader_buffer;
+    ComPtr<ID3DBlob> m_pixel_shader_buffer;
 
-        pixel_shader_buffer = CompileShader(device, pixel, "main", "ps_5_0");
-        ASSERT_SUCCEEDED(device->CreatePixelShader(pixel_shader_buffer->GetBufferPointer(), pixel_shader_buffer->GetBufferSize(), nullptr, &pixel_shader));
-
-        D3D11_INPUT_ELEMENT_DESC layout[] =
-        {
-            { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(DX11Mesh::Vertex, position), D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(DX11Mesh::Vertex, normal), D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, offsetof(DX11Mesh::Vertex, texCoords), D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { "TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, offsetof(DX11Mesh::Vertex, tangent), D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        };
-
-        ASSERT_SUCCEEDED(device->CreateInputLayout(layout, ARRAYSIZE(layout), vertex_shader_buffer->GetBufferPointer(),
-            vertex_shader_buffer->GetBufferSize(), &input_layout));
-
-        ParseVariable(device, ShaderType::kVertex, vertex_shader_buffer, vs_buffers);
-        ParseVariable(device, ShaderType::kPixel, pixel_shader_buffer, ps_buffers);
-    }
+    std::map<std::string, CBuffer> m_vs_buffers;
+    std::map<std::string, CBuffer> m_ps_buffers;
 };
