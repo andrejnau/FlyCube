283b073e87266a856dc3e8beb306691cbbf8785d
Andrew Naumov <softmob@mail.ru>
2017-11-09 08:16:28 +0300

    fix imgui state

--- src/Apps/D3D11/ImGuiPass.cpp
+++ src/Apps/D3D11/ImGuiPass.cpp
@@ -302,8 +302,8 @@ void ImGuiPass::OnUpdate()
 
     ImGuiIO& io = ImGui::GetIO();
 
-    bool show_test_window = true;
-    bool show_another_window = false;
+    static bool show_test_window = true;
+    static bool show_another_window = false;
     static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     {
