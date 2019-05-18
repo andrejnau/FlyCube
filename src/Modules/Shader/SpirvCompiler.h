b164a04254e5c940c96ff966d2bd59819b24efe7
softmob <softmob@mail.ru>
2019-05-18 23:39:03 +0300

    Fixed warning

--- src/Modules/Shader/SpirvCompiler.h
+++ src/Modules/Shader/SpirvCompiler.h
@@ -9,7 +9,7 @@ struct SpirvOption
     bool invert_y = true;
     bool auto_map_bindings = false;
     bool hlsl_iomap = false;
-    uint32_t resource_set_binding = -1u;
+    uint32_t resource_set_binding = ~0u;
     bool use_dxc = false;
 };
 
