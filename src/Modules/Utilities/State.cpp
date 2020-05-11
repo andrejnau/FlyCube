aea660ad38870fd4b76cef2b0ac56ccf5b40c06a
softmob <softmob@mail.ru>
2020-05-11 12:51:55 +0300

    add State.cpp

---
+++ src/Modules/Utilities/State.cpp
@@ -0,0 +1,8 @@
+#include "Utilities/State.h"
+
+CurState::CurState()
+    : vsync(true)
+    , force_dxil(false)
+    , required_gpu_index(-1)
+{
+}
--- src/Modules/Utilities/State.h
+++ src/Modules/Utilities/State.h
@@ -1,13 +1,13 @@
 #pragma once
-
 #include "Utilities/Singleton.h"
 #include <string>
-#include <map>
 
 struct CurState : public Singleton<CurState>
 {
-    bool vsync = true;
-    bool force_dxil = false;
-    uint32_t required_gpu_index = -1;
+    CurState();
+
+    bool vsync;
+    bool force_dxil;
+    uint32_t required_gpu_index;
     std::string gpu_name;
 };
