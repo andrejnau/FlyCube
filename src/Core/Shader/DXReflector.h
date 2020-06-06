419ad7591519db25182a8e40804b27185613271c
softmob <softmob@mail.ru>
2020-06-06 18:43:10 +0300

    update dxc headers

--- src/Core/Shader/DXReflector.h
+++ src/Core/Shader/DXReflector.h
@@ -2,8 +2,6 @@
 #include <d3dcompiler.h>
 #include <dxc/dxcapi.h>
 
-#define D3D_SIT_RTACCELERATIONSTRUCTURE 12
-
 HRESULT DXReflect(
     _In_reads_bytes_(SrcDataSize) LPCVOID pSrcData,
     _In_ SIZE_T SrcDataSize,
