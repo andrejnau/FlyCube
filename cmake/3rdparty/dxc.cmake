41db519dc77fd4a982c5d9ccb5eacc8b6e65b5bc
softmob <softmob@mail.ru>
2021-03-05 22:42:01 +0300

    use PREPEND for CMAKE_MODULE_PATH

--- cmake/3rdparty/dxc.cmake
+++ cmake/3rdparty/dxc.cmake
@@ -4,7 +4,7 @@ target_compile_definitions(dxc INTERFACE DXC_CUSTOM_LOCATION="${project_root}/3r
 set(dxc_default_location "C:/Program Files (x86)/Windows Kits/10/Bin/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/x64")
 target_compile_definitions(dxc INTERFACE DXC_DEFAULT_LOCATION="${dxc_default_location}")
 
-list(APPEND CMAKE_MODULE_PATH "${project_root}/3rdparty/dxc/cmake/modules")
+list(PREPEND CMAKE_MODULE_PATH "${project_root}/3rdparty/dxc/cmake/modules")
 find_package(DiaSDK REQUIRED)
 add_library(dia INTERFACE)
 target_include_directories(dia INTERFACE "${DIASDK_INCLUDE_DIRS}")
