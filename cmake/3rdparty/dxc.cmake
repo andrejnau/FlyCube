828afd4ee68ab78a89b0d3cdad643e9f34d9ec58
THISISAGOODNAME <yangyj19931231@gmail.com>
2021-09-25 16:59:17 +0800

    Add CI

--- cmake/3rdparty/dxc.cmake
+++ cmake/3rdparty/dxc.cmake
@@ -1,7 +1,11 @@
 add_library(dxc INTERFACE)
 target_include_directories(dxc INTERFACE "${project_root}/3rdparty/dxc/include")
 target_compile_definitions(dxc INTERFACE DXC_CUSTOM_LOCATION="${project_root}/3rdparty/dxc/bin")
-set(dxc_default_location "C:/Program Files (x86)/Windows Kits/10/Bin/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/x64")
+if(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION)
+  set(dxc_default_location "C:/Program Files (x86)/Windows Kits/10/Bin/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/x64")
+else()
+  set(dxc_default_location "C:/Program Files (x86)/Windows Kits/10/Redist/D3D/x64") # work around for clion/ninja etc.
+endif()
 target_compile_definitions(dxc INTERFACE DXC_DEFAULT_LOCATION="${dxc_default_location}")
 
 list(PREPEND CMAKE_MODULE_PATH "${project_root}/3rdparty/dxc/cmake/modules")
