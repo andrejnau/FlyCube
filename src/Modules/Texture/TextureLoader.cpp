7efa913a5026debc93c0cf15495e89f79a406691
THISISAGOODNAME <yangyj19931231@gmail.com>
2021-09-28 23:54:20 +0800

    Add KTX support

--- src/Modules/Texture/TextureLoader.cpp
+++ src/Modules/Texture/TextureLoader.cpp
@@ -47,10 +47,33 @@ std::shared_ptr<Resource> CreateSRVFromFileDDS(RenderDevice& device, RenderComma
     return res;
 }
 
+std::shared_ptr<Resource> CreateSRVFromFileKTX(RenderDevice& device, RenderCommandList& command_list, const std::string& path)
+{
+    gli::texture Texture = gli::load(path);
+    auto format = Texture.format();
+    uint32_t width = Texture.extent(0).x;
+    uint32_t height = Texture.extent(0).y;
+    size_t mip_levels = Texture.levels();
+
+    std::shared_ptr<Resource> res = device.CreateTexture(BindFlag::kShaderResource | BindFlag::kCopyDest, format, 1, width, height, 1, mip_levels);
+
+    for (std::size_t level = 0; level < mip_levels; ++level)
+    {
+        size_t row_bytes = 0;
+        size_t num_bytes = 0;
+        GetFormatInfo(Texture.extent(level).x, Texture.extent(level).y, format, num_bytes, row_bytes);
+        command_list.UpdateSubresource(res, level, Texture.data(0, 0, level), row_bytes, num_bytes);
+    }
+
+    return res;
+}
+
 std::shared_ptr<Resource> CreateTexture(RenderDevice& device, RenderCommandList& command_list, const std::string& path)
 {
     if (path.find(".dds") != -1)
         return CreateSRVFromFileDDS(device, command_list, path);
+    else if(path.find(".ktx") != -1)
+        return CreateSRVFromFileKTX(device, command_list, path);
     else
         return CreateSRVFromFile(device, command_list, path);
 }
