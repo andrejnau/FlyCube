f28ffd02f3acaf9969560c98c8c6e37baa320580
Andrew Naumov <softmob@mail.ru>
2021-12-04 18:40:23 +0300

    Fix compilation on macOS

--- cmake/3rdparty/dxc.cmake
+++ cmake/3rdparty/dxc.cmake
@@ -1,12 +1,16 @@
 add_library(dxc INTERFACE)
 target_include_directories(dxc INTERFACE "${project_root}/3rdparty/dxc/include")
 target_compile_definitions(dxc INTERFACE DXC_CUSTOM_LOCATION="${project_root}/3rdparty/dxc/bin")
-if(CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION)
+if (CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION)
     set(dxc_default_location "C:/Program Files (x86)/Windows Kits/10/Bin/${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}/x64")
 else()
     set(dxc_default_location "C:/Program Files (x86)/Windows Kits/10/Redist/D3D/x64") # work around for clion/ninja etc.
 endif()
 target_compile_definitions(dxc INTERFACE DXC_DEFAULT_LOCATION="${dxc_default_location}")
+
+if (APPLE)
+    target_compile_definitions(dxc INTERFACE __EMULATE_UUID)
+endif()
 
 if (WIN32)
     list(PREPEND CMAKE_MODULE_PATH "${project_root}/3rdparty/dxc/cmake/modules")
