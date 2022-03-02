1603bf5704859310a6aece0681e224aeeab19a37
Andrew Naumov <softmob@mail.ru>
2022-03-02 09:37:11 +0300

    Implement GetExecutablePath on macOS

--- src/Modules/Utilities/SystemUtils.cpp
+++ src/Modules/Utilities/SystemUtils.cpp
@@ -1,20 +1,32 @@
 #include "Utilities/SystemUtils.h"
-#ifdef _WIN32
+
+#if defined(_WIN32)
 #include <Windows.h>
+#elif defined(__APPLE__)
+#include <mach-o/dyld.h>
 #else
 #include <linux/limits.h>
 #include <unistd.h>
 #include <stdlib.h>
 #endif
+
 #include <nowide/convert.hpp>
 #include <vector>
 
 std::string GetExecutablePath()
 {
-#ifdef _WIN32
+#if defined(_WIN32)
     wchar_t buf[MAX_PATH] = {};
     GetModuleFileNameW(nullptr, buf, std::size(buf));
     return nowide::narrow(buf);
+#elif defined(__APPLE__)
+    uint32_t buf_size = 0;
+    if (_NSGetExecutablePath(nullptr, &buf_size) != -1)
+        return {};
+    std::vector<char> buf(buf_size);
+    if (_NSGetExecutablePath(buf.data(), &buf_size) != 0)
+        return {};
+    return buf.data();
 #else
     char buf[PATH_MAX] = {};
     readlink("/proc/self/exe", buf, std::size(buf) - 1);
