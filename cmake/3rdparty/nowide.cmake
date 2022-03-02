387b2ffef3f0b5938bde059f9dacd1f28e1e2c34
Andrew Naumov <softmob@mail.ru>
2022-03-02 10:40:58 +0300

    Fix folder properties for Xcode

--- cmake/3rdparty/nowide.cmake
+++ cmake/3rdparty/nowide.cmake
@@ -1,2 +1,7 @@
+set(BUILD_TESTING_SAVED "${BUILD_TESTING}")
+set(BUILD_TESTING OFF)
+
 add_subdirectory(${project_root}/3rdparty/nowide nowide EXCLUDE_FROM_ALL)
 set_target_properties(nowide PROPERTIES FOLDER "3rdparty/nowide")
+
+set(BUILD_TESTING "${BUILD_TESTING_SAVED}")
