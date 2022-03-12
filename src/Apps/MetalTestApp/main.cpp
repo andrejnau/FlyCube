41fb72330a02760b3255e4085b561ef206437413
Andrew Naumov <softmob@mail.ru>
2022-03-12 17:50:55 +0300

    Update MetalTestApp

--- src/Apps/MetalTestApp/main.cpp
+++ src/Apps/MetalTestApp/main.cpp
@@ -1,6 +1,12 @@
 #include <AppBox/AppBox.h>
 #include <AppBox/ArgsParser.h>
-#include <Instance/Instance.h>
+#include <Geometry/Geometry.h>
+#include <Camera/Camera.h>
+#include <Utilities/FormatHelper.h>
+#include <ProgramRef/PixelShader.h>
+#include <ProgramRef/VertexShader.h>
+#include <glm/gtx/transform.hpp>
+#include <stdexcept>
 
 int main(int argc, char* argv[])
 {
@@ -8,98 +14,78 @@ int main(int argc, char* argv[])
     AppBox app("MetalTestApp", settings);
     AppRect rect = app.GetAppRect();
 
-    std::shared_ptr<Instance> instance = CreateInstance(settings.api_type);
-    std::shared_ptr<Adapter> adapter = std::move(instance->EnumerateAdapters()[settings.required_gpu_index]);
-    app.SetGpuName(adapter->GetName());
-    std::shared_ptr<Device> device = adapter->CreateDevice();
-    std::shared_ptr<CommandQueue> command_queue = device->GetCommandQueue(CommandListType::kGraphics);
-    constexpr uint32_t frame_count = 3;
-    std::shared_ptr<Swapchain> swapchain = device->CreateSwapchain(app.GetWindow(), rect.width, rect.height, frame_count, settings.vsync);
-    uint64_t fence_value = 0;
-    std::shared_ptr<Fence> fence = device->CreateFence(fence_value);
+    std::shared_ptr<RenderDevice> device = CreateRenderDevice(settings, app.GetWindow());
+    app.SetGpuName(device->GetGpuName());
 
-    std::vector<uint32_t> index_data = { 0, 1, 2 };
-    std::shared_ptr<Resource> index_buffer = device->CreateBuffer(BindFlag::kIndexBuffer | BindFlag::kCopyDest, sizeof(uint32_t) * index_data.size());
-    index_buffer->CommitMemory(MemoryType::kUpload);
-    index_buffer->UpdateUploadBuffer(0, index_data.data(), sizeof(index_data.front()) * index_data.size());
+    auto dsv = device->CreateTexture(BindFlag::kDepthStencil, gli::format::FORMAT_D32_SFLOAT_PACK32, 1, rect.width, rect.height, 1);
+    auto sampler = device->CreateSampler({
+        SamplerFilter::kAnisotropic,
+        SamplerTextureAddressMode::kWrap,
+        SamplerComparisonFunc::kNever
+    });
 
-    std::vector<glm::vec3> vertex_data = { glm::vec3(-0.5, -0.5, 0.0), glm::vec3(0.0,  0.5, 0.0), glm::vec3(0.5, -0.5, 0.0) };
-    std::shared_ptr<Resource> vertex_buffer = device->CreateBuffer(BindFlag::kVertexBuffer | BindFlag::kCopyDest, sizeof(vertex_data.front()) * vertex_data.size());
-    vertex_buffer->CommitMemory(MemoryType::kUpload);
-    vertex_buffer->UpdateUploadBuffer(0, vertex_data.data(), sizeof(vertex_data.front()) * vertex_data.size());
+    ViewDesc sampler_view_desc = {};
+    sampler_view_desc.bindless = true;
+    sampler_view_desc.view_type = ViewType::kSampler;
+    auto sampler_view = device->CreateView(sampler, sampler_view_desc);
 
-    glm::vec4 constant_data = glm::vec4(1, 0, 0, 1);
-    std::shared_ptr<Resource> constant_buffer = device->CreateBuffer(BindFlag::kConstantBuffer | BindFlag::kCopyDest, sizeof(constant_data));
-    constant_buffer->CommitMemory(MemoryType::kUpload);
-    constant_buffer->UpdateUploadBuffer(0, &constant_data, sizeof(constant_data));
+    Camera camera;
+    camera.SetCameraPos(glm::vec3(-3.0, 2.75, 0.0));
+    camera.SetCameraYaw(-178.0f);
+    camera.SetCameraYaw(-1.75f);
+    camera.SetViewport(rect.width, rect.height);
 
-    std::shared_ptr<Shader> vertex_shader = device->CompileShader({ ASSETS_PATH"shaders/MetalTestApp/VertexShader.hlsl", "main", ShaderType::kVertex, "6_0" });
-    std::shared_ptr<Shader> pixel_shader = device->CompileShader({ ASSETS_PATH"shaders/MetalTestApp/PixelShader.hlsl", "main",  ShaderType::kPixel, "6_0" });
-    std::shared_ptr<Program> program = device->CreateProgram({ vertex_shader, pixel_shader });
+    std::shared_ptr<RenderCommandList> upload_command_list = device->CreateRenderCommandList();
 
-    ViewDesc constant_view_desc = {};
-    constant_view_desc.view_type = ViewType::kConstantBuffer;
-    constant_view_desc.dimension = ViewDimension::kBuffer;
-    std::shared_ptr<View> constant_view = device->CreateView(constant_buffer, constant_view_desc);
-    BindKey settings_key = { ShaderType::kPixel, ViewType::kConstantBuffer, 0, 0, 1 };
-    std::shared_ptr<BindingSetLayout> layout = device->CreateBindingSetLayout({ settings_key });
-    std::shared_ptr<BindingSet> binding_set = device->CreateBindingSet(layout);
-    binding_set->WriteBindings({ { settings_key, constant_view } });
+    Model model(*device, *upload_command_list, ASSETS_PATH"model/export3dcoat/export3dcoat.obj");
+    model.matrix = glm::scale(glm::vec3(0.1f)) * glm::translate(glm::vec3(0.0f, 30.0f, 0.0f)) * glm::rotate(glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
-    RenderPassDesc render_pass_desc = {
-        { { swapchain->GetFormat(), RenderPassLoadOp::kClear, RenderPassStoreOp::kStore } },
-    };
-    std::shared_ptr<RenderPass> render_pass = device->CreateRenderPass(render_pass_desc);
-    ClearDesc clear_desc = { { { 0.0, 0.2, 0.4, 1.0 } } };
-    GraphicsPipelineDesc pipeline_desc = {
-        program,
-        layout,
-        { { 0, "POSITION", gli::FORMAT_RGB32_SFLOAT_PACK32, sizeof(vertex_data.front()) } },
-        render_pass
-    };
-    std::shared_ptr<Pipeline> pipeline = device->CreateGraphicsPipeline(pipeline_desc);
+    ProgramHolder<PixelShader, VertexShader> program(*device);
+    program.vs.cbuffer.ConstantBuf.model = glm::transpose(model.matrix);
+    program.vs.cbuffer.ConstantBuf.view = glm::transpose(camera.GetViewMatrix());
+    program.vs.cbuffer.ConstantBuf.projection = glm::transpose(camera.GetProjectionMatrix());
+    program.vs.cbuffer.ConstantBuf.normalMatrix = glm::transpose(glm::transpose(glm::inverse(model.matrix)));
 
-    std::array<uint64_t, frame_count> fence_values = {};
-    std::vector<std::shared_ptr<CommandList>> command_lists;
-    std::vector<std::shared_ptr<Framebuffer>> framebuffers;
-    for (uint32_t i = 0; i < frame_count; ++i)
+    upload_command_list->Close();
+    device->ExecuteCommandLists({ upload_command_list });
+
+    std::vector<std::shared_ptr<RenderCommandList>> command_lists;
+    for (uint32_t i = 0; i < settings.frame_count; ++i)
     {
-        ViewDesc back_buffer_view_desc = {};
-        back_buffer_view_desc.view_type = ViewType::kRenderTarget;
-        back_buffer_view_desc.dimension = ViewDimension::kTexture2D;
-        std::shared_ptr<Resource> back_buffer = swapchain->GetBackBuffer(i);
-        std::shared_ptr<View> back_buffer_view = device->CreateView(back_buffer, back_buffer_view_desc);
-        FramebufferDesc framebuffer_desc = {};
-        framebuffer_desc.render_pass = render_pass;
-        framebuffer_desc.width = rect.width;
-        framebuffer_desc.height = rect.height;
-        framebuffer_desc.colors = { back_buffer_view };
-        std::shared_ptr<Framebuffer> framebuffer = framebuffers.emplace_back(device->CreateFramebuffer(framebuffer_desc));
-        std::shared_ptr<CommandList> command_list = command_lists.emplace_back(device->CreateCommandList(CommandListType::kGraphics));
-        command_list->BindPipeline(pipeline);
-        command_list->BindBindingSet(binding_set);
+        RenderPassBeginDesc render_pass_desc = {};
+        render_pass_desc.colors[0].texture = device->GetBackBuffer(i);
+        render_pass_desc.colors[0].clear_color = { 1.0f, 1.0f, 1.0f, 1.0f };
+        render_pass_desc.depth_stencil.texture = dsv;
+        render_pass_desc.depth_stencil.clear_depth = 1.0;
+
+        decltype(auto) command_list = device->CreateRenderCommandList();
+        command_list->UseProgram(program);
         command_list->SetViewport(0, 0, rect.width, rect.height);
-        command_list->SetScissorRect(0, 0, rect.width, rect.height);
-        command_list->IASetIndexBuffer(index_buffer, gli::format::FORMAT_R32_UINT_PACK32);
-        command_list->IASetVertexBuffer(0, vertex_buffer);
-        command_list->ResourceBarrier({ { back_buffer, ResourceState::kPresent, ResourceState::kRenderTarget } });
-        command_list->BeginRenderPass(render_pass, framebuffer, clear_desc);
-        command_list->DrawIndexed(3, 1, 0, 0, 0);
+        command_list->Attach(program.vs.cbv.ConstantBuf, program.vs.cbuffer.ConstantBuf);
+        model.ia.indices.Bind(*command_list);
+        model.ia.positions.BindToSlot(*command_list, program.vs.ia.POSITION);
+        model.ia.normals.BindToSlot(*command_list, program.vs.ia.NORMAL);
+        model.ia.texcoords.BindToSlot(*command_list, program.vs.ia.TEXCOORD);
+        model.ia.tangents.BindToSlot(*command_list, program.vs.ia.TANGENT);
+
+        command_list->BeginRenderPass(render_pass_desc);
+        for (auto& range : model.ia.ranges)
+        {
+            auto& material = model.GetMaterial(range.id);
+            command_list->Attach(program.ps.srv.albedoMap, material.texture.albedo);
+            command_list->DrawIndexed(range.index_count, 1, range.start_index_location, range.base_vertex_location, 0);
+        }
         command_list->EndRenderPass();
-        command_list->ResourceBarrier({ { back_buffer, ResourceState::kRenderTarget, ResourceState::kPresent } });
+
         command_list->Close();
+        command_lists.emplace_back(command_list);
     }
 
     while (!app.PollEvents())
     {
-        uint32_t frame_index = swapchain->NextImage(fence, ++fence_value);
-        command_queue->Wait(fence, fence_value);
-        fence->Wait(fence_values[frame_index]);
-        command_queue->ExecuteCommandLists({ command_lists[frame_index] });
-        command_queue->Signal(fence, fence_values[frame_index] = ++fence_value);
-        swapchain->Present(fence, fence_values[frame_index]);
+        device->ExecuteCommandLists({ command_lists[device->GetFrameIndex()] });
+        device->Present();
     }
-    command_queue->Signal(fence, ++fence_value);
-    fence->Wait(fence_value);
+    device->WaitForIdle();
     return 0;
 }
