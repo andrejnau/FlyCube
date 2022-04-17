6b1198b908af5585c54ce167dabb77461f4c606d
softmob <softmob@mail.ru>
2022-04-17 19:36:59 +0300

    Fix build on Windows

--- cmake/3rdparty/moltenvk.cmake
+++ cmake/3rdparty/moltenvk.cmake
@@ -1,2 +1,6 @@
+if (NOT METAL_SUPPORT)
+    return()
+endif()
+
 add_subdirectory("${project_root}/3rdparty/MVKPixelFormats")
 set_target_properties(MVKPixelFormats PROPERTIES FOLDER "3rdparty/MoltenVK")
