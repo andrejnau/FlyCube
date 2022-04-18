987caa9663f04e51906f90e863f3aa057def64f8
Andrew Naumov <softmob@mail.ru>
2022-04-18 21:24:59 +0300

    Rename MVKPixelFormats cmake

--- cmake/3rdparty/mvkpixelformats.cmake
+++ cmake/3rdparty/mvkpixelformats.cmake
@@ -3,4 +3,4 @@ if (NOT METAL_SUPPORT)
 endif()
 
 add_subdirectory("${project_root}/3rdparty/MVKPixelFormats")
-set_target_properties(MVKPixelFormats PROPERTIES FOLDER "3rdparty/MoltenVK")
+set_target_properties(MVKPixelFormats PROPERTIES FOLDER "3rdparty/MVKPixelFormats")
