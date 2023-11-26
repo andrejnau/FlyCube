e2e3d790217536140f69c0f5869eef4ff1ec0b47
Andrew Naumov <softmob@mail.ru>
2023-11-26 14:52:21 +0100

    Fix build from vscode on macos

--- cmake/3rdparty/vulkan.cmake
+++ cmake/3rdparty/vulkan.cmake
@@ -3,34 +3,63 @@ add_subdirectory(${project_root}/3rdparty/Vulkan-Headers Vulkan-Headers EXCLUDE_
 add_library(vulkan INTERFACE)
 target_link_libraries(vulkan INTERFACE Vulkan-Headers)
 
+if (NOT VULKAN_SUPPORT)
+    return()
+endif()
+
 if (APPLE)
     set_property(TARGET vulkan APPEND PROPERTY
         INTERFACE_COMPILE_DEFINITIONS
             USE_STATIC_MOLTENVK
     )
+
+    if (DEFINED ENV{VULKAN_SDK})
+        message("VULKAN_SDK is $ENV{VULKAN_SDK}")
+    else()
+        message(FATAL_ERROR "VULKAN_SDK is not set")
+    endif()
+    
     set(moltenvk_xcframework "$ENV{VULKAN_SDK}/MoltenVK/MoltenVK.xcframework")
-    set(moltenvk_copy_dir "${CMAKE_CURRENT_BINARY_DIR}/moltenvk")
-    set(moltenvk_copy_status "${moltenvk_copy_dir}/status")
-    add_custom_command(OUTPUT "${moltenvk_copy_status}"
-        COMMAND cmake -E make_directory "${moltenvk_copy_dir}"
-        COMMAND cmake -E create_symlink "${moltenvk_xcframework}/ios-arm64/libMoltenVK.a" "${moltenvk_copy_dir}/iOS-iphoneos-libMoltenVK.a"
-        COMMAND cmake -E create_symlink "${moltenvk_xcframework}/ios-arm64_x86_64-simulator/libMoltenVK.a" "${moltenvk_copy_dir}/iOS-iphonesimulator-libMoltenVK.a"
-        COMMAND cmake -E create_symlink "${moltenvk_xcframework}/macos-arm64_x86_64/libMoltenVK.a" "${moltenvk_copy_dir}/Darwin-libMoltenVK.a"
-        COMMAND cmake -E create_symlink "${moltenvk_xcframework}/tvos-arm64_arm64e/libMoltenVK.a" "${moltenvk_copy_dir}/tvOS-appletvos-libMoltenVK.a"
-        COMMAND cmake -E create_symlink "${moltenvk_xcframework}/tvos-arm64_x86_64-simulator/libMoltenVK.a" "${moltenvk_copy_dir}/tvOS-appletvsimulator-libMoltenVK.a"
-        COMMAND cmake -E touch "${moltenvk_copy_status}"
+
+    add_library(vulkan_libs_iOS-iphoneos UNKNOWN IMPORTED)
+    set_property(TARGET vulkan_libs_iOS-iphoneos APPEND PROPERTY
+        IMPORTED_LOCATION
+            "${moltenvk_xcframework}/ios-arm64/libMoltenVK.a"
     )
-    add_custom_target(moltenvk_copy_ckeck DEPENDS ${moltenvk_copy_status})
-    add_dependencies(vulkan moltenvk_copy_ckeck)
+
+    add_library(vulkan_libs_iOS-iphonesimulator UNKNOWN IMPORTED)
+    set_property(TARGET vulkan_libs_iOS-iphonesimulator APPEND PROPERTY
+        IMPORTED_LOCATION
+            "${moltenvk_xcframework}/ios-arm64_x86_64-simulator/libMoltenVK.a"
+    )
+
+    add_library(vulkan_libs_Darwin UNKNOWN IMPORTED)
+    set_property(TARGET vulkan_libs_Darwin APPEND PROPERTY
+        IMPORTED_LOCATION
+            "${moltenvk_xcframework}/macos-arm64_x86_64/libMoltenVK.a"
+    )
+
+    add_library(vulkan_libs_tvOS-appletvos UNKNOWN IMPORTED)
+    set_property(TARGET vulkan_libs_tvOS-appletvos APPEND PROPERTY
+        IMPORTED_LOCATION
+            "${moltenvk_xcframework}/tvos-arm64_arm64e/libMoltenVK.a"
+    )
+
+    add_library(vulkan_libs_tvOS-appletvsimulator UNKNOWN IMPORTED)
+    set_property(TARGET vulkan_libs_tvOS-appletvsimulator APPEND PROPERTY
+        IMPORTED_LOCATION
+            "${moltenvk_xcframework}/tvos-arm64_x86_64-simulator/libMoltenVK.a"
+    )
+
     if (CMAKE_GENERATOR STREQUAL "Xcode")
         set_property(TARGET vulkan APPEND PROPERTY
             INTERFACE_LINK_LIBRARIES
-                "${moltenvk_copy_dir}/$<PLATFORM_ID>$(EFFECTIVE_PLATFORM_NAME)-libMoltenVK.a"
+                "vulkan_libs_$<PLATFORM_ID>$(EFFECTIVE_PLATFORM_NAME)"
         )
     else()
         set_property(TARGET vulkan APPEND PROPERTY
             INTERFACE_LINK_LIBRARIES
-                "${moltenvk_copy_dir}/$<PLATFORM_ID>-libMoltenVK.a"
+                "vulkan_libs_$<PLATFORM_ID>"
         )
     endif()
     set_property(TARGET vulkan APPEND PROPERTY
