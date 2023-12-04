703e159e0ca7f6aa609cff79a4ad69f7aa393321
Andrew Naumov <softmob@mail.ru>
2023-12-04 21:09:24 +0100

    Remove unused code

--- src/Modules/Utilities/DXGIFormatHelper.h
+++ src/Modules/Utilities/DXGIFormatHelper.h
@@ -1,17 +1,6 @@
 #pragma once
 
 #include <directx/dxgiformat.h>
-#include <gli/gli.hpp>
-
-//--------------------------------------------------------------------------------------
-// Get surface information for a particular format
-//--------------------------------------------------------------------------------------
-void GetSurfaceInfo(size_t width,
-                    size_t height,
-                    gli::format format,
-                    size_t* outNumBytes,
-                    size_t* outRowBytes,
-                    size_t* outNumRows);
 
 DXGI_FORMAT MakeTypelessDepthStencil(DXGI_FORMAT format);
 bool IsTypelessDepthStencil(DXGI_FORMAT format);
