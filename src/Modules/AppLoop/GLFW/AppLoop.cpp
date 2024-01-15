871c4ef06df8644e5fcbf0fcd558526e534206e2
Andrew Naumov <andrejnau@gmail.com>
2024-01-15 10:39:59 +0100

    Do not rely on AppBox impl in AppLoop

--- src/Modules/AppLoop/GLFW/AppLoop.cpp
+++ src/Modules/AppLoop/GLFW/AppLoop.cpp
@@ -1,9 +1,52 @@
 #include "AppLoop/AppLoop.h"
 
-#include "AppBox/AppBox.h"
-#include "AppBox/ArgsParser.h"
+#if defined(_WIN32)
+#define GLFW_EXPOSE_NATIVE_WIN32
+#elif defined(__APPLE__)
+#define GLFW_EXPOSE_NATIVE_COCOA
+#else
+#define GLFW_EXPOSE_NATIVE_X11
+#endif
+
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 
 #include <cassert>
+
+#if defined(__APPLE__)
+#import <QuartzCore/QuartzCore.h>
+#endif
+
+namespace {
+
+AppSize GetScreenSize()
+{
+    auto* monitor_desc = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    return AppSize(monitor_desc->width, monitor_desc->height);
+}
+
+AppSize GetDefaultWindowsSize(const AppSize& screen_size)
+{
+    return AppSize(screen_size.width() / 1.5, screen_size.height() / 1.5);
+}
+
+void* GetNativeWindow(GLFWwindow* window)
+{
+#if defined(_WIN32)
+    return (void*)glfwGetWin32Window(window);
+#elif defined(__APPLE__)
+    NSWindow* ns_window = glfwGetCocoaWindow(window);
+    if (!ns_window.contentView.layer) {
+        ns_window.contentView.layer = [CAMetalLayer layer];
+        ns_window.contentView.wantsLayer = YES;
+    }
+    return (__bridge void*)ns_window.contentView.layer;
+#else
+    return (void*)glfwGetX11Window(window);
+#endif
+}
+
+} // namespace
 
 // static
 AppLoop& AppLoop::GetInstance()
@@ -20,12 +63,27 @@ AppRenderer& AppLoop::GetRendererImpl()
 
 int AppLoop::RunImpl(std::unique_ptr<AppRenderer> renderer, int argc, char* argv[])
 {
-    Settings settings = ParseArgs(argc, argv);
-    AppBox app_box(renderer->GetTitle(), settings);
-    renderer->Init(app_box.GetAppSize(), app_box.GetNativeWindow());
+    glfwInit();
 
-    while (!app_box.PollEvents()) {
+    AppSize screen_size = GetScreenSize();
+    AppSize window_size = GetDefaultWindowsSize(screen_size);
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window =
+        glfwCreateWindow(window_size.width(), window_size.height(), renderer->GetTitle().data(), nullptr, nullptr);
+
+    int xpos = (screen_size.width() - window_size.width()) / 2;
+    int ypos = (screen_size.height() - window_size.height()) / 2;
+    glfwSetWindowPos(window, xpos, ypos);
+
+    renderer->Init(window_size, GetNativeWindow(window));
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
         renderer->Render();
     }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
