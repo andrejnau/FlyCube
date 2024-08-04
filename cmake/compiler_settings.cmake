6a266ed2d5b5d462dc1f292e7ad6c6db5218a523
Andrew Naumov <andrejnau@gmail.com>
2024-08-04 13:58:43 +0200

    Compile warning as error

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -1,12 +1,27 @@
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_COMPILE_WARNING_AS_ERROR ON)
 
 if (MSVC)
     add_compile_options("/MP")
     add_definitions(-DUNICODE -D_UNICODE)
     add_definitions(-D_CRT_SECURE_NO_WARNINGS)
     add_definitions(-DNOMINMAX)
+    add_compile_options(
+        /wd4005
+        /wd4715
+        /wd4834
+        /wd4838
+    )
+else()
+    add_compile_options(
+        $<$<COMPILE_LANGUAGE:CXX>:-Wno-deprecated-declarations>
+        $<$<COMPILE_LANGUAGE:CXX>:-Wno-shorten-64-to-32>
+        $<$<COMPILE_LANGUAGE:CXX>:-Wno-switch>
+        $<$<COMPILE_LANGUAGE:CXX>:-Wno-unguarded-availability-new>
+        $<$<COMPILE_LANGUAGE:CXX>:-Wno-unused-result>
+    )
 endif()
 
 if (CMAKE_SYSTEM_NAME STREQUAL "iOS")
