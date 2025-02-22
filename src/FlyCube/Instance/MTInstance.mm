90132cb9d34b234fe8915984278cfd1dd4598169
Andrew Naumov <andrejnau@gmail.com>
2025-02-22 23:03:00 +0100

    Remove setenv METAL_DEBUG_ERROR_MODE
    Setting this variable has no effect anymore.
    Use `MTL_DEBUG_LAYER=1 MTL_DEBUG_LAYER_ERROR_MODE=nslog` instead.
    See `man MetalValidation` for more information.

--- src/FlyCube/Instance/MTInstance.mm
+++ src/FlyCube/Instance/MTInstance.mm
@@ -4,10 +4,7 @@
 
 #import <Metal/Metal.h>
 
-MTInstance::MTInstance()
-{
-    setenv("METAL_DEBUG_ERROR_MODE", "5", 1);
-}
+MTInstance::MTInstance() = default;
 
 std::vector<std::shared_ptr<Adapter>> MTInstance::EnumerateAdapters()
 {
