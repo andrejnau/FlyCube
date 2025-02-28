44c32f865c2f653a2cb0be82410bf9cd52a478b5
Andrew Naumov <andrejnau@gmail.com>
2025-02-28 10:35:20 +0100

    Use sections in HLSLCompilerTest

--- src/FlyCube/HLSLCompiler/test/main.cpp
+++ src/FlyCube/HLSLCompiler/test/main.cpp
@@ -5,50 +5,47 @@
 
 void RunTest(const ShaderDesc& desc)
 {
-    auto dxil_blob = Compile(desc, ShaderBlobType::kDXIL);
-    REQUIRE(!dxil_blob.empty());
+    SECTION("DXIL")
+    {
+        auto dxil_blob = Compile(desc, ShaderBlobType::kDXIL);
+        REQUIRE(!dxil_blob.empty());
+    }
 
-    auto spirv_blob = Compile(desc, ShaderBlobType::kSPIRV);
-    REQUIRE(!spirv_blob.empty());
+    SECTION("SPIRV")
+    {
+        auto spirv_blob = Compile(desc, ShaderBlobType::kSPIRV);
+        REQUIRE(!spirv_blob.empty());
 
-    if (desc.type != ShaderType::kLibrary) {
-        std::map<std::string, uint32_t> mapping;
-        auto source = GetMSLShader(spirv_blob, mapping);
-        REQUIRE(!source.empty());
+        SECTION("MSL")
+        {
+            if (desc.type != ShaderType::kLibrary) {
+                std::map<std::string, uint32_t> mapping;
+                auto source = GetMSLShader(spirv_blob, mapping);
+                REQUIRE(!source.empty());
+            }
+        }
     }
 }
 
-TEST_CASE("TriangleVS")
+TEST_CASE("HLSLCompilerTest")
 {
-    RunTest({ ASSETS_PATH "shaders/Triangle/VertexShader.hlsl", "main", ShaderType::kVertex, "6_3" });
-}
-
-TEST_CASE("TrianglePS")
-{
-    RunTest({ ASSETS_PATH "shaders/Triangle/PixelShader.hlsl", "main", ShaderType::kPixel, "6_3" });
-}
-
-TEST_CASE("RayTracing")
-{
-    RunTest({ ASSETS_PATH "shaders/DxrTriangle/RayTracing.hlsl", "", ShaderType::kLibrary, "6_3" });
-}
-
-TEST_CASE("RayTracingHit")
-{
-    RunTest({ ASSETS_PATH "shaders/DxrTriangle/RayTracingHit.hlsl", "", ShaderType::kLibrary, "6_3" });
-}
-
-TEST_CASE("RayTracingCallable")
-{
-    RunTest({ ASSETS_PATH "shaders/DxrTriangle/RayTracingCallable.hlsl", "", ShaderType::kLibrary, "6_3" });
-}
-
-TEST_CASE("MeshTriangleMS")
-{
-    RunTest({ ASSETS_PATH "shaders/MeshTriangle/MeshShader.hlsl", "main", ShaderType::kMesh, "6_5" });
-}
-
-TEST_CASE("MeshTrianglePS")
-{
-    RunTest({ ASSETS_PATH "shaders/MeshTriangle/PixelShader.hlsl", "main", ShaderType::kPixel, "6_5" });
+    std::vector<ShaderDesc> shader_descs = {
+        { ASSETS_PATH "shaders/Bindless/PixelShader.hlsl", "main", ShaderType::kPixel, "6_0" },
+        { ASSETS_PATH "shaders/Bindless/VertexShader.hlsl", "main", ShaderType::kVertex, "6_0" },
+        { ASSETS_PATH "shaders/DxrTriangle/RayTracing.hlsl", "", ShaderType::kLibrary, "6_3" },
+        { ASSETS_PATH "shaders/DxrTriangle/RayTracingCallable.hlsl", "", ShaderType::kLibrary, "6_3" },
+        { ASSETS_PATH "shaders/DxrTriangle/RayTracingHit.hlsl", "", ShaderType::kLibrary, "6_3" },
+        { ASSETS_PATH "shaders/MeshTriangle/MeshShader.hlsl", "main", ShaderType::kMesh, "6_5" },
+        { ASSETS_PATH "shaders/MeshTriangle/PixelShader.hlsl", "main", ShaderType::kPixel, "6_5" },
+        { ASSETS_PATH "shaders/Triangle/PixelShader.hlsl", "main", ShaderType::kPixel, "6_0" },
+        { ASSETS_PATH "shaders/Triangle/PixelShaderNoBindings.hlsl", "main", ShaderType::kPixel, "6_0" },
+        { ASSETS_PATH "shaders/Triangle/VertexShader.hlsl", "main", ShaderType::kVertex, "6_0" },
+    };
+    for (const auto& shader_desc : shader_descs) {
+        auto test_name = shader_desc.shader_path.substr(std::string_view{ ASSETS_PATH }.size());
+        DYNAMIC_SECTION(test_name)
+        {
+            RunTest(shader_desc);
+        }
+    }
 }
