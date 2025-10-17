6000c07bef30a92652e8a4b08c9ad609381c3063
Andrew Naumov <andrejnau@gmail.com>
2025-10-17 17:13:18 +0200

    Fix glfw folder cmake properties

--- cmake/3rdparty/glfw.cmake
+++ cmake/3rdparty/glfw.cmake
@@ -9,5 +9,6 @@ option(GLFW_INSTALL "Generate installation target" OFF)
 
 add_subdirectory(${project_root}/3rdparty/glfw glfw EXCLUDE_FROM_ALL)
 set_target_properties(glfw PROPERTIES FOLDER "3rdparty/glfw")
+set_target_properties(update_mappings PROPERTIES FOLDER "3rdparty/glfw")
 target_include_directories(glfw INTERFACE "${project_root}/3rdparty/glfw/include")
 target_compile_definitions(glfw INTERFACE GLFW_INCLUDE_NONE)
