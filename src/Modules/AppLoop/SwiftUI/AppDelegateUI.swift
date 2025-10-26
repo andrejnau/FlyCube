8b29d35f36dfae9e6e5db72705bcb088853235c5
Andrew Naumov <andrejnau@gmail.com>
2025-10-26 12:53:01 +0100

    Fix AppDelegateUI.swift for iOS 26

--- src/Modules/AppLoop/SwiftUI/AppDelegateUI.swift
+++ src/Modules/AppLoop/SwiftUI/AppDelegateUI.swift
@@ -5,7 +5,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
+        let scene = application.connectedScenes.first as! UIWindowScene
+        window = UIWindow(windowScene: scene)
         window?.rootViewController = UIHostingController(rootView: ContentView())
         window?.makeKeyAndVisible()
         return true
