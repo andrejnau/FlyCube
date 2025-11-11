2e6413360d0bc67c262e59c8af1a55c0c45ccd3f
Andrew Naumov <andrejnau@gmail.com>
2025-11-11 13:45:41 +0100

    Disable unused assimp importers

--- cmake/3rdparty/assimp.cmake
+++ cmake/3rdparty/assimp.cmake
@@ -1,17 +1,10 @@
-option (ASSIMP_BUILD_ASSIMP_TOOLS
-    "If the supplementary tools for Assimp are built in addition to the library."
-    OFF
-)
-
-option (ASSIMP_BUILD_TESTS
-    "If the test suite for Assimp is built in addition to the library."
-    OFF
-)
-
-option (ASSIMP_NO_EXPORT
-    "Disable Assimp's export functionality."
-    ON
-)
+set(ASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT OFF)
+set(ASSIMP_BUILD_GLTF_IMPORTER ON)
+set(ASSIMP_BUILD_OBJ_IMPORTER ON)
+set(ASSIMP_BUILD_TESTS OFF)
+set(ASSIMP_INSTALL OFF)
+set(ASSIMP_INSTALL_PDB OFF)
+set(ASSIMP_NO_EXPORT ON)
 
 add_subdirectory(${project_root}/3rdparty/assimp assimp EXCLUDE_FROM_ALL)
 
@@ -19,20 +12,4 @@ set_target_properties(assimp PROPERTIES FOLDER "3rdparty/assimp")
 
 if (TARGET zlibstatic)
     set_target_properties(zlibstatic PROPERTIES FOLDER "3rdparty/assimp")
-endif()
-
-if (TARGET uninstall)
-set_target_properties(uninstall PROPERTIES FOLDER "3rdparty/assimp")
-endif()
-
-if (TARGET UpdateAssimpLibsDebugSymbolsAndDLLs)
-    set_target_properties(UpdateAssimpLibsDebugSymbolsAndDLLs PROPERTIES FOLDER "3rdparty/assimp")
-endif()
-
-if (TARGET IrrXML)
-set_target_properties(IrrXML PROPERTIES FOLDER "3rdparty/assimp")
-endif()
-
-if (TARGET zlib)
-    set_target_properties(zlib PROPERTIES FOLDER "3rdparty/assimp")
 endif()
